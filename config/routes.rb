aa40821aba74cd351be04223be58eb14a4fc9c01
kunihitouehara <uekuni2000@gmail.com>
2021-07-30 18:12:37 +0900

    root設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: "posts#index"#TOP
   resources :posts
     resources :sessions, only: [:new, :create, :destroy]
     resources :users, only: [:new, :create,:show]
