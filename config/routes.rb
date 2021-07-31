6568fdb1b14def09f48149fceba62c8a689af97a
kunihitouehara <uekuni2000@gmail.com>
2021-07-31 20:57:39 +0900

    画像確認画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
     resources :sessions, only: [:new, :create, :destroy]
     resources :users, only: [:new, :create,:show]
     resources :favorites, only: [:create, :destroy]
-    resources :posts
-    #collection do
-      #post:confirm
-    #end
-    #end
+    resources :posts do
+      collection do 
+        post :confirm
+      end
+    end
 end
